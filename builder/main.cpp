57895591eb0dad1d525754fc671a5b45c1370394
Bendi11 <bkliebmann@gmail.com>
2021-02-15 17:46:09 -0800

    Deleted dear imgui demo

--- builder/main.cpp
+++ builder/main.cpp
@@ -8,6 +8,10 @@
 neural::set trainingData;
 
 #include <SDL.h>
+#include <imgui.h>
+#include <imgui_impl_opengl3.h>
+#include <imgui_impl_sdl.h>
+#include <glad/glad.h>
 
 
 int main(int argc, char* argv[])
